829d84ae0736a37c1c6582374f868e92410b3fb7
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-17 19:16:00 -0400

    Added schema for remaining tables

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -17,7 +17,47 @@ CREATE TABLE products (
   default_price SMALLINT
 );
 
+CREATE TABLE features (
+  id SMALLINT PRIMARY KEY,
+  product_id SMALLINT,
+  feature VARCHAR(75),
+  value VARCHAR(75),
+  CONSTRAINT fk_feat FOREIGN KEY(product_id) REFERENCES products(id)
+);
 
+CREATE TABLE styles (
+  id SMALLINT PRIMARY KEY,
+  productId SMALLINT,
+  name VARCHAR(75),
+  sale_price SMALLINT,
+  original_price SMALLINT,
+  default_style BOOLEAN,
+  CONSTRAINT fk_style FOREIGN KEY(product_id) REFERENCES products(id)
+);
+
+CREATE TABLE photos (
+  id SMALLINT PRIMARY KEY,
+  styleId SMALLINT,
+  url VARCHAR(255),
+  thumbnail_url VARCHAR(255),
+  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id)
+)
+
+CREATE TABLE skus (
+  id SMALLINT PRIMARY KEY,
+  styleId SMALLINT,
+  size VARCHAR(3),
+  quantity SMALLINT,
+  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id)
+)
+
+CREATE TABLE related (
+  id SMALLINT PRIMARY KEY,
+  current_product_id SMALLINT,
+  related_product_id SMALLINT,
+  CONSTRAINT fk_current FOREIGN KEY(current_product_id) REFERENCES products(id),
+  CONSTRAINT fk_related FOREIGN KEY(related_product_id) REFERENCES products(id)
+);
 
 --  psql -U your_user -d your_db -f filename.sql
 --  psql -d sdc_test -f schema.sql [from postgres folder - put into an npm script ? :)]
