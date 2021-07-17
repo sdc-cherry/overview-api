8edc5708b2f51882600d26d0403bf5f9b4744bd0
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-17 19:28:11 -0400

    Initialized RDBMS

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE features (
   product_id SMALLINT,
   feature VARCHAR(75),
   value VARCHAR(75),
-  CONSTRAINT fk_feat FOREIGN KEY(product_id) REFERENCES products(id)
+  CONSTRAINT fk_feat FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
 CREATE TABLE styles (
@@ -32,7 +32,7 @@ CREATE TABLE styles (
   sale_price SMALLINT,
   original_price SMALLINT,
   default_style BOOLEAN,
-  CONSTRAINT fk_style FOREIGN KEY(product_id) REFERENCES products(id)
+  CONSTRAINT fk_style FOREIGN KEY(productId) REFERENCES products(id) ON DELETE CASCADE
 );
 
 CREATE TABLE photos (
@@ -40,24 +40,24 @@ CREATE TABLE photos (
   styleId SMALLINT,
   url VARCHAR(255),
   thumbnail_url VARCHAR(255),
-  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id)
-)
+  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id) ON DELETE CASCADE
+);
 
 CREATE TABLE skus (
   id SMALLINT PRIMARY KEY,
   styleId SMALLINT,
   size VARCHAR(3),
   quantity SMALLINT,
-  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id)
-)
+  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id) ON DELETE CASCADE
+);
 
 CREATE TABLE related (
   id SMALLINT PRIMARY KEY,
   current_product_id SMALLINT,
   related_product_id SMALLINT,
-  CONSTRAINT fk_current FOREIGN KEY(current_product_id) REFERENCES products(id),
-  CONSTRAINT fk_related FOREIGN KEY(related_product_id) REFERENCES products(id)
+  CONSTRAINT fk_current FOREIGN KEY(current_product_id) REFERENCES products(id) ON DELETE CASCADE,
+  CONSTRAINT fk_related FOREIGN KEY(related_product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
 --  psql -U your_user -d your_db -f filename.sql
---  psql -d sdc_test -f schema.sql [from postgres folder - put into an npm script ? :)]
+--  \psql -d sdc -f schema.sql [from postgres folder - put into an npm script ? :)]
