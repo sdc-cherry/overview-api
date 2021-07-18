a8d44f057a2686932faac401cfc1bacd477064fb
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-17 20:36:38 -0400

    Added copy statements to schema

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -17,47 +17,59 @@ CREATE TABLE products (
   default_price INTEGER
 );
 
+\COPY products from '~/Documents/HR/sdc/overview-api/raw/product.csv' delimiter ',' csv header;
+
 CREATE TABLE features (
-  id SMALLINT PRIMARY KEY,
-  product_id SMALLINT,
+  id INTEGER PRIMARY KEY,
+  product_id INTEGER,
   feature VARCHAR(75),
   value VARCHAR(75),
   CONSTRAINT fk_feat FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
+\COPY features from '~/Documents/HR/sdc/overview-api/raw/features.csv' delimiter ',' csv header;
+
 CREATE TABLE styles (
-  id SMALLINT PRIMARY KEY,
-  productId SMALLINT,
+  id INTEGER PRIMARY KEY,
+  productId INTEGER,
   name VARCHAR(75),
-  sale_price INTEGER,
+  sale_price INTEGER DEFAULT NULL,
   original_price INTEGER,
   default_style BOOLEAN,
   CONSTRAINT fk_style FOREIGN KEY(productId) REFERENCES products(id) ON DELETE CASCADE
 );
 
+\COPY styles from '~/Documents/HR/sdc/overview-api/raw/styles.csv' delimiter ',' csv header;
+
 CREATE TABLE photos (
-  id SMALLINT PRIMARY KEY,
-  styleId SMALLINT,
+  id INTEGER PRIMARY KEY,
+  styleId INTEGER,
   url VARCHAR(255),
   thumbnail_url VARCHAR(255),
   CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id) ON DELETE CASCADE
 );
 
+\COPY photos from '~/Documents/HR/sdc/overview-api/raw/photos.csv' delimiter ',' csv header;
+
 CREATE TABLE skus (
-  id SMALLINT PRIMARY KEY,
-  styleId SMALLINT,
+  id INTEGER PRIMARY KEY,
+  styleId INTEGER,
   size VARCHAR(3),
   quantity SMALLINT,
   CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id) ON DELETE CASCADE
 );
 
+\COPY skus from '~/Documents/HR/sdc/overview-api/raw/skus.csv' delimiter ',' csv header;
+
 CREATE TABLE related (
-  id SMALLINT PRIMARY KEY,
-  current_product_id SMALLINT,
-  related_product_id SMALLINT,
+  id INTEGER PRIMARY KEY,
+  current_product_id INTEGER,
+  related_product_id INTEGER,
   CONSTRAINT fk_current FOREIGN KEY(current_product_id) REFERENCES products(id) ON DELETE CASCADE,
   CONSTRAINT fk_related FOREIGN KEY(related_product_id) REFERENCES products(id) ON DELETE CASCADE
 );
+
+\COPY related from '~/Documents/HR/sdc/overview-api/raw/related.csv' delimiter ',' csv header;
 
 --  psql -U your_user -d your_db -f filename.sql
 --  \psql -d sdc -f schema.sql [from postgres folder - put into an npm script ? :)]
