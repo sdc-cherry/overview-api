d3f5aedd92c5e9c0d14b6e1961238a998d516acd
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-20 00:12:19 -0400

    Basic API tests for response values

--- __tests__/example.js
+++ __tests__/example.js
@@ -1,14 +1,24 @@
 
-// const app = require('../server/index');
-// const supertest = require('supertest');
+const app = require('../server/index');
+const supertest = require('supertest');
+
+// beforeEach(() => { app = require('../server/index'); });
+// afterEach(() => { app.close(); });
 
 test('Example', () => {
   expect(1+1).toEqual(2);
 });
 
-// test('Supertest', async () => {
-//   await supertest(app).get('/products')
-//     .then(res => {
-//       expect(res.text).toEqual('products');
-//     })
-// })
+test('Supertest', async () => {
+  await supertest(app).get('/products')
+    .then(res => {
+      expect(res.text).toEqual('products');
+    })
+})
+
+test('Supertest 2', async () => {
+  await supertest(app).get('/products/1')
+    .then(res => {
+      expect(res.text).toEqual('information');
+    })
+})
--- postgres/index.js
+++ postgres/index.js
@@ -1,24 +1,24 @@
 const { Pool, Client } = require('pg');
 const config = require('../config.js');
 
-const client = new Client({
+// const client = new Client({
+//   user: config.DBUSER,
+//   database: config.DBNAME,
+//   password: config.DBTOKEN,
+//   port: 5432,
+// })
+const pool = new Pool({
   user: config.DBUSER,
   database: config.DBNAME,
   password: config.DBTOKEN,
   port: 5432,
 })
 
-client.connect();
 
+// client.connect();
 
 let query = 'SELECT * FROM cart LEFT JOIN sess ON cart.session_id=sess.session_id';
 
-const test = () => {
-  return client.query(query)
-    .then(res => {
-      client.end();
-      return res;
-    });
-}
+const test = () => pool.query(query);
 
 module.exports.test = test;
