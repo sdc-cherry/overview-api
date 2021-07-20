8461860fa840f391675fdf775c1e2941e6c56ca7
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-19 22:09:16 -0400

    Added API routes with documentation and example responses

--- server/index.js
+++ server/index.js
@@ -3,17 +3,61 @@ const db = require('../postgres/index');
 const app = express();
 
 
+// Test query
+// app.get('/', (req, res, next) => {
+//   db.test()
+//     .then(res => {
+//       console.log('rows: ', res.rows);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+// })
 
-app.get('/', (req, res, next) => {
-  db.test()
-    .then(res => {
-      console.log('rows: ', res.rows);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+// List Products
+app.get('/products', (req, res) => {
+  /**
+   * Parameters:
+   *    page (integer) - Selects the page of results to return. Default 1.
+   *    count (integer) - Specifies how many results per page to return. Default 5.
+   *
+   * Response status: 200 OK
+   */
+  res.send('products');
 })
 
+// Product Information
+app.get('/products/:product_id', (req, res) => {
+  /**
+   * Parameters:
+   *    product_id (integer) - Required ID of the Product requested
+   *
+   * Response status: 200 OK
+   */
+  res.send('information');
+})
+
+// Product Styles
+app.get('/products/:product_id/styles', (req, res) => {
+  /**
+   * Parameters:
+   *    product_id (integer) - Required ID of the Product requested
+   *
+   * Response status: 200 OK
+   */
+  res.send('styles');
+})
+
+// Related Products
+app.get('/products/:product_id/related', (req, res) => {
+  /**
+   * Parameters:
+   *    product_id (integer) - Required ID of the Product requested
+   *
+   * Response status: 200 OK
+   */
+  res.send('related');
+})
 
 
 app.listen(3000, function () {
