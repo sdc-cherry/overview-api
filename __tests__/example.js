a39c1caef7e5fe0062781d10f9b3a317a36c1579
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-20 00:38:23 -0400

    Some ideas for API testing

--- __tests__/example.js
+++ __tests__/example.js
@@ -5,6 +5,15 @@ const supertest = require('supertest');
 // beforeEach(() => { app = require('../server/index'); });
 // afterEach(() => { app.close(); });
 
+// .set('Accept', 'application/json')
+// .auth('username', 'password')
+
+// content type (supertest docs)
+// content length (same)
+// .expect(200) - also test incorrect params for error response code
+// maybe response.body.length for results parameter
+
+
 test('Example', () => {
   expect(1+1).toEqual(2);
 });
