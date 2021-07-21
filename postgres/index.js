217620654d053b2912e1795009ba8365b7ed2b1a
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-20 23:42:22 -0400

    Refactored db file to define db functions as export methods

--- postgres/index.js
+++ postgres/index.js
@@ -8,9 +8,9 @@ const pool = new Pool({
   port: 5432,
 })
 
-const list = numResults => pool.query(`SELECT * FROM products LIMIT ${numResults}`);
+module.exports.list = numResults => pool.query(`SELECT * FROM products LIMIT ${numResults}`);
 
-const info = id => {
+module.exports.info = id => {
   return pool.query(`SELECT * FROM products WHERE id=${id}`)
     .then(result1 => {
       return pool.query(`SELECT feature, value FROM features WHERE product_id=${id}`)
@@ -22,8 +22,4 @@ const info = id => {
     });
 }
 
-const related = id => pool.query(`SELECT related_product_id FROM related WHERE current_product_id=${id}`);
-
-module.exports.list = list;
-module.exports.info = info;
-module.exports.related = related;
+module.exports.related = id => pool.query(`SELECT related_product_id FROM related WHERE current_product_id=${id}`);
