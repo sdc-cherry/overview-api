56981fbeff06b9079b8ae467eb382e3b3ffca034
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-07-23 09:26:58 -0400

    Added multiple query attempts to styles route and ran test coverage

--- postgres/index.js
+++ postgres/index.js
@@ -24,37 +24,61 @@ module.exports.info = id => {
 };
 
 module.exports.styles = id => {
-  // Use ARRAY_AGG  - https://www.postgresqltutorial.com/postgresql-aggregate-functions/postgresql-array_agg-function/
-  let query = `SELECT s.id AS style_id, s.name, s.sale_price, s.original_price, s.default_style, JSON_AGG(p) AS photos
-                FROM styles AS s LEFT JOIN photos AS p ON s.id=p.styleId
-                WHERE productId=${id}
-                GROUP BY s.id`
-  //Query styles table info
-  return pool.query(query)
-    // .then(styleObjs => {
-    //   // result1 is an array of style objects
-    //   // Map array of style objects to promise-style queries
-    //   let allStyleCalls = styleObjs.map(style => {
-    //     console.log(style);
-    //     // For each style ID (style object)
-    //       // rename id -> style_id
-    //       style.style_id = style.id;
-    //       // get photos for that style ID
-    //       return pool.query()
-    //         // Set resulting array as property on style object
-    //       // get skus for that style ID JSON_OBJECT_AGG
-    //         // Set resulting array as AN OBJECT property on style object
-    //     return Promise.resolve(style);
 
-    //   })
-    //   Promise.all(allStyleCalls).then(allStyles => {
-    //     // Define results object
-    //     // Set all results as a property in results object
-    //     // Define a property with the current (input) product ID
-    //     // Return results object
-    //     return allStyles;
-    //   })
-    // })
+  // JSON_OBJECT_AGG(quantity, size) -> quantity value is key and size - probably want when each sku obj built and need skus
+
+  // For each sku (id),
+    // want to construct an object with quantity and size properties
+
+  // Want object with all of the sku id: sku object properties
+
+  // let query = `SELECT JSON_OBJECT_AGG(quantity, size) AS test
+  //               FROM skus
+  //               WHERE styleID=1
+  //               GROUP BY styleID`
+  // let query = `SELECT id, JSON_OBJECT_AGG(quantity, size) AS test
+  //               FROM skus
+  //               WHERE styleID=1
+  //               GROUP BY id`
+
+  //sku data for single style id
+  // let query = `SELECT *
+  //               FROM skus
+  //               WHERE styleID=1
+  //               GROUP BY id`
+
+  // PHOTOS AS ARRAY OF OBJECT, BUT WITH EXTRA FIELDS
+  // let query = `SELECT s.id AS style_id, s.name, s.sale_price, s.original_price, s.default_style, JSON_AGG(p) AS photos
+  //               FROM styles AS s
+  //                 LEFT JOIN photos AS p
+  //                   ON s.id=p.styleId
+  //               WHERE productId=${id}
+  //               GROUP BY s.id`;
+
+  // GIVES DUPLICATED ARRAYS, BUT WORKABLE - 4 s
+  // let query = `SELECT s.id AS style_id, s.name, s.sale_price, s.original_price, s.default_style, JSON_AGG(p) AS photos, JSON_AGG(u) AS skus
+  //               FROM styles AS s
+  //                 LEFT JOIN photos AS p
+  //                   ON s.id=p.styleId
+  //                 INNER JOIN skus AS u
+  //                   ON s.id=u.styleId
+  //               WHERE productId=${id}
+  //               GROUP BY s.id`;
+
+  // Try replacing LEFT JOIN photos with LJ (qry statement to get styleid and array of objs)
+  let query = `SELECT s.id AS style_id, s.name, s.sale_price, s.original_price, s.default_style, p.url, p.thumbnail_url, u.id AS sku_id, u.quantity, u.size
+                FROM styles AS s
+                  LEFT JOIN photos AS p
+                    ON s.id=p.styleId
+                  INNER JOIN skus AS u
+                    ON s.id=u.styleId
+                WHERE productId=${id}`;
+
+  // JUST STYLES
+  // let query = `SELECT s.id AS style_id, s.name, s.sale_price, s.original_price, s.default_style
+  //               FROM styles AS s
+  //               WHERE productId=${id}`;
+  return pool.query(query)
 };
 
 module.exports.related = id => pool.query(`SELECT related_product_id FROM related WHERE current_product_id=${id}`);
--- postgres/schema.example.sql
+++ postgres/schema.example.sql
@@ -1,6 +1,8 @@
 
 DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS sess;
+DROP TABLE IF EXISTS styles;
+DROP TABLE IF EXISTS photos;
 
 
 -- CREATE SCHEMA sdc
@@ -17,11 +19,35 @@ CREATE TABLE cart(
   CONSTRAINT fk_sess FOREIGN KEY(session_id) REFERENCES sess(session_id)
 );
 
+\COPY cart from '~/Documents/HR/sdc/overview-api/raw/cart.csv' delimiter ',' csv header;
+
 INSERT INTO sess(session_id, session_name)
   VALUES
     (1234, 'session 1'),
     (4321, 'session 2'),
     (1111, 'session 3');
 
+CREATE TABLE styles (
+  id INTEGER PRIMARY KEY,
+  productId INTEGER,
+  name VARCHAR(75),
+  photos JSON
+);
+
+CREATE TABLE photos(
+  id INTEGER PRIMARY KEY,
+  styleId INTEGER,
+  photoObj JSON,
+  CONSTRAINT fk_photo FOREIGN KEY(styleId) REFERENCES styles(id) ON DELETE CASCADE
+);
+
+INSERT INTO styles(id, productId, name)
+  VALUES
+    (1,1,'style1','[ { "url": "style1photo1", "thumbnail_url":"style1photo1thumb" }, { "url": "style1photo2", "thumbnail_url":"style1photo2thumb" } ]'),
+    (2,1,'style2','[ { "url": "style2photo1", "thumbnail_url":"style2photo1thumb" }, { "url": "style2photo2", "thumbnail_url":"style2photo2thumb" } ]');
+
+-- INSERT INTO photos(id, styleId, photoObj)
+  -- VALUES
+  --   ()
 --  psql -U your_user -d your_db -f filename.sql
 --  psql -d sdc_test -f schema.sql [from postgres folder - put into an npm script ? :)]
