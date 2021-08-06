8a4107ddf696cff8b73aedacb681e845c78fe36d
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-08-05 23:02:15 -0400

    Changed local /products stress test to 100 VUs for 60 seconds

--- k6/local.list.js
+++ k6/local.list.js
@@ -3,7 +3,7 @@ import { sleep, check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 1 }
+    { duration: '60s', target: 100 }
     // { duration: '1m30s', target: 10 },
     // { duration: '20s', target: 0 },
   ],
