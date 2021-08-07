32a6e41c268c933ab3f1d80c32a0b509999153ce
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-08-07 11:14:50 -0400

    Added indexes for styles query

--- postgres/indexing.sql
+++ postgres/indexing.sql
@@ -2,12 +2,16 @@
 
 DROP INDEX IF EXISTS product_id_index;
 DROP INDEX IF EXISTS features_id_index;
-
+DROP INDEX IF EXISTS styles_multicol_index;
+DROP INDEX IF EXISTS skus_styleid_index;
+DROP INDEX IF EXISTS photos_styleid_index;
 DROP INDEX IF EXISTS related_current_index;
 
 CREATE INDEX product_id_index ON products (id);
 CREATE INDEX features_id_index ON features (product_id);
--- styles, sku, and photo indexing
+CREATE INDEX styles_multicol_index ON styles (productId,id);
+CREATE INDEX skus_styleid_index ON skus (styleId);
+CREATE INDEX photos_styleid_index ON photos (styleId);
 CREATE INDEX related_current_index ON related (current_product_id);
 
 -- TO RUN:
