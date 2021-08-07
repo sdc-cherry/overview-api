aba95ffbaf23790deeb2d7a0db6802b55a556b24
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-08-07 00:36:04 -0400

    Added index for products id field

--- postgres/indexing.sql
+++ postgres/indexing.sql
@@ -1,12 +1,16 @@
 
+
+DROP INDEX IF EXISTS product_id_index;
+DROP INDEX IF EXISTS features_id_index;
+
 DROP INDEX IF EXISTS related_id_index;
 DROP INDEX IF EXISTS related_current_index;
 DROP INDEX IF EXISTS related_related_index;
 
--- CREATE INDEX related_id_index ON related (id);
+CREATE INDEX product_id_index ON products (id);
+-- CREATE INDEX features_id_index ON features (product_id);
+-- styles, sku, and photo indexing
 CREATE INDEX related_current_index ON related (current_product_id);
--- CREATE INDEX related_related_index ON related (related_product_id);
-
 
 -- TO RUN:
 -- \psql -d sdc -f postgres/indexing.sql
