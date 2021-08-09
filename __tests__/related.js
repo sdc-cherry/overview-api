b65539300b682261893b996ddbe6e9f53c58d1f0
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-08-09 14:42:49 -0400

    Added /related test for deep ID

--- __tests__/related.js
+++ __tests__/related.js
@@ -8,3 +8,12 @@ test('Valid request for related products should return an array', () => {
       expect(Array.isArray(res.body)).toBeTruthy();
     })
 })
+
+test('Valid request for product 999990 should return 5 related products', () => {
+   return supertest(app).get('/products/999990/related')
+    .then(res => {
+      expect(res.status).toEqual(200);
+      expect(Array.isArray(res.body)).toBeTruthy();
+      expect(res.body.length).toEqual(5);
+    })
+})
