92796e6b726b293592d7a8a2717ea595ac4df639
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-08-09 14:40:39 -0400

    Added /styles test for deep ID

--- __tests__/styles.js
+++ __tests__/styles.js
@@ -15,6 +15,19 @@ test('Valid request for product with photos, skus should return arrays for those
   });
 });
 
+test('Valid request for product 999999 should return three style objects. Final object should have four photos and six SKUs',  () => {
+  return supertest(app).get('/products/999999/styles')
+  .then(res => {
+    expect(res.status).toEqual(200);
+    expect(Array.isArray(res.body)).toBeTruthy();
+    expect(typeof(res.body[0]["skus"])).toEqual('object');
+    expect(Array.isArray(res.body[0]["photos"])).toBeTruthy();
+    expect(res.body.length).toEqual(3);
+    expect(res.body[2]["photos"].length).toEqual(4);
+    expect(Object.keys(res.body[2]["skus"]).length).toEqual(6);
+  });
+});
+
 // No photos or skus
 test('Valid request for product with no photos, skus should return empty arrays for those properties',  () => {
   return supertest(app).get('/products/2/styles')
